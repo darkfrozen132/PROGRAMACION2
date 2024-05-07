2d3e189cf63a60b92ef81d1783b375618d582f39
darkfrozen132 <darkfrozen@outlook.com>
2024-05-07 08:13:39 -0500

    asdas

---
+++ practica_funciones_punteros/2023_2/ListaConRegistros.h
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   ListaConRegistros.h
+ * Author: leoncio
+ *
+ * Created on 2 de mayo de 2024, 09:11 AM
+ */
+
+#ifndef LISTACONREGISTROS_H
+#define LISTACONREGISTROS_H
+
+
+
+#endif /* LISTACONREGISTROS_H */
+
